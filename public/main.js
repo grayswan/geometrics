fd1cd03693e1e36431c8496a55b7cf52781cbbc1
Kristen Grashel <kristen@spatialnetworks.com>
2018-05-28 23:31:12 -0400

    add gradient and a bunch of other stuff

--- public/main.js
+++ public/main.js
@@ -1,41 +1,81 @@
 $(document).ready(() => {
+  var name = anime.timeline()
+  name
+    .add({
+      targets: '#first-name .k.el',
+      translateY: '15vh',
+      easing: 'easeOutExpo',
+      offset: 500,
+      opacity: [0, 1]
+    })
+    .add({
+      targets: '#first-name .r.el',
+      translateY: '15vh',
+      easing: 'easeOutExpo',
+      offset: 600,
+      opacity: [0, 1]
+    })
+    .add({
+      targets: '#first-name .i.el',
+      translateY: '15vh',
+      easing: 'easeOutExpo',
+      offset: 700,
+      opacity: [0, 1]
+    })
+    .add({
+      targets: '#first-name .s.el',
+      translateY: '15vh',
+      easing: 'easeOutExpo',
+      offset: 800,
+      opacity: [0, 1]
+    })
+    .add({
+      targets: '#first-name .t.el',
+      translateY: '15vh',
+      easing: 'easeOutExpo',
+      offset: 900,
+      opacity: [0, 1]
+    })
+    .add({
+      targets: '#first-name .e.el',
+      translateY: '15vh',
+      easing: 'easeOutExpo',
+      offset: 1000,
+      opacity: [0, 1]
+    })
+    .add({
+      targets: '#first-name .n.el',
+      translateY: '15vh',
+      easing: 'easeOutExpo',
+      offset: 1100,
+      opacity: [0, 1]
+    })
+    .add({
+      targets: '#last-name .swan',
+      translateY: '-35vh',
+      easing: 'easeOutExpo',
+      offset: 1200,
+      duration: 3000,
+      opacity: [0, 1]
+    })
+    .add({
+      targets: '#last-name .grashel',
+      translateY: '-35vh',
+      easing: 'easeOutExpo',
+      offset: 1200,
+      duration: 3000,
+      opacity: [0, 1]
+    })
 
-  var CSStransforms = anime({
-    targets: '#top .el',
-    rotate: '1turn',
-    translateY: '15vh',
-    easing: 'easeOutQuad',
-    borderTopColor: '#C4094E',
-    scale: .50,
-    delay: 2000
-  });
-
-  var CSStransforms = anime({
-    targets: '#right .el',
-    translateY: '50vh',
-    translateX: '-25vw',
-    rotate: '.75turn',
-    easing: 'easeOutQuad',
-    borderRightColor: '#E5E5E5',
-    scale: .25,
-    delay: 2000
-  });
-
-  var CSStransforms = anime({
-    targets: '#bottom .el',
-    translateY: '-5vh',
-    rotate: '.5turn',
-    easing: 'easeOutQuad',
-    borderBottomColor: '#FF4444',
-    delay: 2000
-  });
-
-  var CSStransforms = anime({
-    targets: '#name .el',
-    translateY: '-35vh',
-    translateX: '30vw',
-    delay: 500,
-    easing: 'easeOutCubic'
-  });
-
+  var coverTransform = anime({
+    targets: '#cover',
+    scale: {
+      value: 10,
+      duration: 1000,
+      easing: 'easeOutQuad'
+    }
+  })
 })
+
+function openCategory() {
+}
